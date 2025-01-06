32bb3ceeb20c52cbe3d8c2dc178ed148913b949e
Sandro <sandro.kohler@edu.tbz.ch>
2025-01-06 11:05:08 +0100

    add feature edit recipe

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,35 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Browse.css';
 import axios from "axios";
 import Recipe from "../Recipe/Recipe";
-import {Col, Row} from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 const baseURL = "http://localhost:8080/api/recipes";
 
 
 const Browse = () => {
 
-        const [post, setPost] = React.useState(null);
+    const [post, setPost] = React.useState(null);
 
-        React.useEffect(() => {
-            axios.get(baseURL).then((response) => {
-                setPost(response.data);
-            });
-        }, []);
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setPost(response.data);
+        });
+    }, []);
 
-        if (!post) return null;
+    if (!post) return null;
 
-        return (
-            <>
-                <Row>
-                    {post.map((d) => (
-                        <Col sm={12} md={6} lg={4} xl={3}>
-                            <Recipe title={d.name} description={d.description} image={d.imageUrl} />
-                        </Col>
-                    ))}
-                </Row>
-            </>
-        );
+    return (
+        <>
+            <Row>
+                {post.map((d) => (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Recipe id={d.id} title={d.name} description={d.description} image={d.imageUrl} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    );
 }
 
 
